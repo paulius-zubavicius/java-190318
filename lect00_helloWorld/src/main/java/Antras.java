10c3d6105ff36c1ed468548f7fff54440df7fb0f
Paulius Zubavičius <paulius@zubavicius.lt>
2019-03-18 15:30:06 +0200

    hello world

---
+++ lect00_helloWorld/src/main/java/Antras.java
@@ -0,0 +1,9 @@
+public class Antras {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+
+}
---
+++ lect00_helloWorld/src/main/java/Pirmas.java
@@ -0,0 +1,9 @@
+public class Pirmas {
+
+    public static void main(String[] args) {
+
+        System.out.println("Labas pasauli");
+
+    }
+
+}
