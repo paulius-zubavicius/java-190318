e8e4ec52c9224ce208012873ddb1f2226b1c522b
Paulius Zubavičius <paulius@zubavicius.lt>
2019-04-05 12:35:02 +0300

    selenium needed improvments

--- l13_unit_tests/src/test/java/com/vcs/lects/l13/unittests/library/DelfiSelenium.java
+++ l13_unit_tests/src/test/java/com/vcs/lects/l13/unittests/library/DelfiSelenium.java
@@ -1,22 +1,32 @@
 package com.vcs.lects.l13.unittests.library;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import com.opera.core.systems.OperaDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DelfiSelenium {
 
 	@Test
-	public void delfiPage() {
+	public void delfiPage() throws InterruptedException {
 
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "/snap/bin/chromium");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
 
-		driver.get("http://www.google.com");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://google.com");
+
+        Thread.sleep(1000);
+
+        if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+                System.out.println("Pass");
+        } else {
+                System.out.println("Fail");
+        }
+        driver.quit();
 
 		//WebElement element = driver.findElement(By.className("headline-title"));
 
