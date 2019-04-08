a85cd9c6adb809fec7a20f4fbd1e42e11608106f
Paulius Zubavičius <paulius@zubavicius.lt>
2019-04-08 12:34:21 +0300

    task for code duplicate

---
+++ l16_spring_dependency_injection/src/main/java/com/vcs/lects/l16/spring/tasks/ApskaitaTask.java
@@ -0,0 +1,70 @@
+package com.vcs.lects.l16.spring.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * Yra kodo dublikatu. Perdarykite, kad ju neliktu
+ * 
+ * 
+ */
+
+public class ApskaitaTask {
+
+	List<Cekis> cekiai = new ArrayList<>();
+
+	public void pridetiCeki(Cekis cekis) {
+		cekiai.add(cekis);
+	}
+
+	public double vidLitraiSimtui() {
+
+		Cekis pirmasCekis = cekiai.get(0);// pirmas cekis
+
+		Cekis paskutinisCekis = cekiai.get(cekiai.size() - 1);// paskutinis cekis
+
+		int nuvaziuotasAtstumas = paskutinisCekis.getOdometroparodymai() - pirmasCekis.getOdometroparodymai();
+
+		double litraiViso = 0;
+		for (int i = 0; i < cekiai.size() - 1; i++) {
+
+			litraiViso = litraiViso + cekiai.get(i).getKiekis();
+
+		}
+
+		double litraiSimtuiKm = litraiViso / nuvaziuotasAtstumas;
+
+		return litraiSimtuiKm * 100.0;
+
+	}
+
+	public double vidKainaSimtui() {
+
+		Cekis pirmasCekis = cekiai.get(0);
+
+		Cekis paskutinisCekis = cekiai.get(cekiai.size() - 1);
+
+		double nuvaziuotasAtstumas = paskutinisCekis.getOdometroparodymai() - pirmasCekis.getOdometroparodymai();
+
+		double litraiViso = 0;
+		for (int i = 0; i < cekiai.size() - 1; i++) {
+
+			litraiViso = litraiViso + cekiai.get(i).getKiekis();
+		}
+
+		double vidLitroKaina = 0;
+		for (int i = 0; i < cekiai.size() - 1; i++) {
+
+			vidLitroKaina = cekiai.get(i).getKainaVnt() + vidLitroKaina;
+		}
+
+		double litraiSimtuiKm = litraiViso / nuvaziuotasAtstumas;
+
+		double bendraLitroKaina = vidLitroKaina / (cekiai.size() - 1);
+		double tikraKaina = bendraLitroKaina * vidLitraiSimtui();
+
+		return tikraKaina;
+	}
+
+}
---
+++ l16_spring_dependency_injection/src/main/java/com/vcs/lects/l16/spring/tasks/Cekis.java
@@ -0,0 +1,63 @@
+package com.vcs.lects.l16.spring.tasks;
+
+public class Cekis {
+
+	private String data;
+	private DegaluRusis degalurusis;
+	private double kainaVnt;
+	private double kiekis;
+	private int odometroparodymai;
+
+	public Cekis(DegaluRusis degalurusis, double kainaVnt, double kiekis, int odometroparodymai) {
+		this.degalurusis = degalurusis;
+		this.kainaVnt = kainaVnt;
+		this.kiekis = kiekis;
+		this.odometroparodymai = odometroparodymai;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public DegaluRusis getDegalurusis() {
+		return degalurusis;
+	}
+
+	public void setDegalurusis(DegaluRusis degalurusis) {
+		this.degalurusis = degalurusis;
+	}
+
+	public double getKainaVnt() {
+		return kainaVnt;
+	}
+
+	public void setKainaVnt(double kainaVnt) {
+		this.kainaVnt = kainaVnt;
+	}
+
+	public double getKiekis() {
+		return kiekis;
+	}
+
+	public void setKiekis(double kiekis) {
+		this.kiekis = kiekis;
+	}
+
+	public int getOdometroparodymai() {
+		return odometroparodymai;
+	}
+
+	public void setOdometroparodymai(int odometroparodymai) {
+		this.odometroparodymai = odometroparodymai;
+	}
+}
+
+enum DegaluRusis {
+
+	D, E95, E98, LPG;
+
+}
